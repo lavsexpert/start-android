eaabcb093520f448d0f98ce95696c6cb29843d22
Lavsexpert <lavrov-sergey@yandex.ru>
2017-12-04 15:58:24 +0300

    А теперь 2 копии View вложены в оба Layout, и они являются root для этих View

--- AndroidLessonsLists/p0401layoutinflater/src/main/java/ru/startandroid/p0401layoutinflater/MainActivity.java
+++ AndroidLessonsLists/p0401layoutinflater/src/main/java/ru/startandroid/p0401layoutinflater/MainActivity.java
@@ -22,19 +22,17 @@ public class MainActivity extends AppCompatActivity {
         LayoutInflater ltInflater = getLayoutInflater();
 
         LinearLayout linLayout = (LinearLayout) findViewById(R.id.linLayout);
-        View view1 = ltInflater.inflate(R.layout.text, linLayout, false);
+        View view1 = ltInflater.inflate(R.layout.text, linLayout, true);
         ViewGroup.LayoutParams lp1 = view1.getLayoutParams();
 
         Log.d(LOG_TAG, "Class of view1: " + view1.getClass().toString());
         Log.d(LOG_TAG, "Class of layoutParams of view1: " + lp1.getClass().toString());
-        Log.d(LOG_TAG, "Text of view1: " + ((TextView) view1).getText());
 
         RelativeLayout relLayout = (RelativeLayout) findViewById(R.id.relLayout);
-        View view2 = ltInflater.inflate(R.layout.text, relLayout, false);
+        View view2 = ltInflater.inflate(R.layout.text, relLayout, true);
         ViewGroup.LayoutParams lp2 = view2.getLayoutParams();
 
         Log.d(LOG_TAG, "Class of view2: " + view2.getClass().toString());
         Log.d(LOG_TAG, "Class of layoutParams of view2: " + lp2.getClass().toString());
-        Log.d(LOG_TAG, "Text of view2: " + ((TextView) view2).getText());
     }
 }
