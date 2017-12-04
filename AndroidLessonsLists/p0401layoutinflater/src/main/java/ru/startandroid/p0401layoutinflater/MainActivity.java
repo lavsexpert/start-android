fb84083a254d4643e37c70b60b55c4a09efd67e2
Lavsexpert <lavrov-sergey@yandex.ru>
2017-12-04 15:49:46 +0300

    Теперь View оказался внутри LinearLayout

--- AndroidLessonsLists/p0401layoutinflater/src/main/java/ru/startandroid/p0401layoutinflater/MainActivity.java
+++ AndroidLessonsLists/p0401layoutinflater/src/main/java/ru/startandroid/p0401layoutinflater/MainActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
         LayoutInflater ltInflater = getLayoutInflater();
         View view = ltInflater.inflate(R.layout.text, null, false);
         ViewGroup.LayoutParams lp = view.getLayoutParams();
+
+        LinearLayout linLayout = (LinearLayout) findViewById(R.id.linLayout);
+        linLayout.addView(view);
 
         Log.d(LOG_TAG, "Class of view: " + view.getClass().toString());
         Log.d(LOG_TAG, "LayoutParams of view is null: " + (lp == null));
