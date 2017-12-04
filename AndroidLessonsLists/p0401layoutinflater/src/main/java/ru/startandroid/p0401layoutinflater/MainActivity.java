2c6934e8c828fb5271af4182e59c27bc4e013650
Lavsexpert <lavrov-sergey@yandex.ru>
2017-12-04 15:51:40 +0300

    Добавили view в оба Layout-а, но не указали root - в итоге view нигде не отобразился

--- AndroidLessonsLists/p0401layoutinflater/src/main/java/ru/startandroid/p0401layoutinflater/MainActivity.java
+++ AndroidLessonsLists/p0401layoutinflater/src/main/java/ru/startandroid/p0401layoutinflater/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,14 +20,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         LayoutInflater ltInflater = getLayoutInflater();
-        View view = ltInflater.inflate(R.layout.text, null, false);
-        ViewGroup.LayoutParams lp = view.getLayoutParams();
 
         LinearLayout linLayout = (LinearLayout) findViewById(R.id.linLayout);
-        linLayout.addView(view);
+        View view1 = ltInflater.inflate(R.layout.text, linLayout, false);
+        ViewGroup.LayoutParams lp1 = view1.getLayoutParams();
 
-        Log.d(LOG_TAG, "Class of view: " + view.getClass().toString());
-        Log.d(LOG_TAG, "LayoutParams of view is null: " + (lp == null));
-        Log.d(LOG_TAG, "Text of view: " + ((TextView) view).getText());
+        Log.d(LOG_TAG, "Class of view1: " + view1.getClass().toString());
+        Log.d(LOG_TAG, "Class of layoutParams of view1: " + lp1.getClass().toString());
+        Log.d(LOG_TAG, "Text of view1: " + ((TextView) view1).getText());
+
+        RelativeLayout relLayout = (RelativeLayout) findViewById(R.id.relLayout);
+        View view2 = ltInflater.inflate(R.layout.text, relLayout, false);
+        ViewGroup.LayoutParams lp2 = view2.getLayoutParams();
+
+        Log.d(LOG_TAG, "Class of view2: " + view2.getClass().toString());
+        Log.d(LOG_TAG, "Class of layoutParams of view2: " + lp2.getClass().toString());
+        Log.d(LOG_TAG, "Text of view2: " + ((TextView) view2).getText());
     }
 }
