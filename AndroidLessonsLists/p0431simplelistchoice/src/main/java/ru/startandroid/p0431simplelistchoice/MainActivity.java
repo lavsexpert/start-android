a23de4f58c3048578560e63f5b6f5db682daddc4
Lavsexpert <lavrov-sergey@yandex.ru>
2017-12-14 14:51:17 +0300

    Урок 43. Одиночный и множественный выбор в ListView. Одиночный выбор.

---
+++ AndroidLessonsLists/p0431simplelistchoice/src/main/java/ru/startandroid/p0431simplelistchoice/MainActivity.java
@@ -0,0 +1,43 @@
+package ru.startandroid.p0431simplelistchoice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    final String LOG_TAG = "myLogs";
+
+    ListView lvMain;
+    String[] names;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lvMain = (ListView) findViewById(R.id.lvMain);
+        // устанавливаем режим выбора пунктов списка
+        lvMain.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        // Создаем адаптер, используя массив из файла ресурсов
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.names, android.R.layout.simple_list_item_single_choice);
+        lvMain.setAdapter(adapter);
+
+        Button btnChecked = (Button) findViewById(R.id.btnChecked);
+        btnChecked.setOnClickListener(this);
+
+        // получаем массив из файла ресурсов
+        names = getResources().getStringArray(R.array.names);
+    }
+
+    public void onClick(View arg0) {
+        // пишем в лог выделенный элемент
+        Log.d(LOG_TAG, "checked: " + names[lvMain.getCheckedItemPosition()]);
+    }
+
+}
